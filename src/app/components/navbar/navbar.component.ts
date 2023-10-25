f2abfb486c65a2f1b1d936676b18163ab4d56732
Acux <juansup123@gmail.com>
2023-10-25 18:29:44 -0500

    navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,6 +6,11 @@ interface User{
   username: string;
 }
 
+interface Users2{
+  username: string;
+  id: number;
+}
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -14,12 +19,14 @@ interface User{
 export class NavbarComponent implements OnInit{
 
   user: any = {}
+  users2: Users2[] = []
 
   constructor(private http: HttpClient,private cookieService: CookieService,){
   }
 
   ngOnInit(): void {
     this.getUser()
+    this.getSolicitudes()
   }
 
   getUser(){
@@ -34,6 +41,21 @@ export class NavbarComponent implements OnInit{
     this.http.get<User>(apiUrl,{headers}).subscribe((data: any) => { // Usamos 'any' para el tipo de la respuesta, ya que no coincide directamente con nuestra interfaz Carta.
         this.user = data
         console.log("hola")
+    });
+  }
+
+  getSolicitudes(){
+    const accessToken = this.cookieService.get('access_token');
+    //este es el encabezado de la peticion
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `JWT ${accessToken}`
+  });
+    const apiUrl = `http://127.0.0.1:8000/api/solicitud/`;
+
+    this.http.get<Users2[]>(apiUrl,{headers}).subscribe((data: Users2[]) => { // Usamos 'any' para el tipo de la respuesta, ya que no coincide directamente con nuestra interfaz Carta.
+        this.user = data
+        console.log(data)
     });
   }
 
